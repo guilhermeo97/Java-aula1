bcc55e42debbe14cc4987398490aa77ea3f5485f
guilhermeo97 <oliveiraguilherme591@gmail.com>
2023-02-13 22:20:10 -0300

    init

---
+++ Hello-world.java
@@ -0,0 +1,15 @@
+class Hello {
+    public static void main (String[] args) {
+    	int inteiro = 34;
+        boolean booleana = true;
+        char caracter = 'C';
+        char codigo = 56;
+        float flutuante = 0.7f;
+        double dobro = 142.6f;
+        String palavra = "Foobar";
+        if(inteiro == 34){
+            System.out.println("Verdadeiro");
+        }
+        System.out.println("Hello, World.");
+    }
+}
